187b561f0279b64d9d21ad7a9f988fb7fdbf3502
M140310\student <hk041280@gmail.com>
2018-04-19 17:10:59 +0900

    modify map function

--- Bowling/app/src/main/java/com/example/student/bowling/MapsActivity.java
+++ Bowling/app/src/main/java/com/example/student/bowling/MapsActivity.java
@@ -1,9 +1,13 @@
 package com.example.student.bowling;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +15,7 @@ import android.webkit.WebView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +23,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,6 +36,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     WebView web_dash;
     FrameLayout game_fm;
     private MarkerOptions mOpt;
+    LocationTask locationTask;
+    Double slat,slog;
 
 
     @Override
@@ -51,12 +61,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
     public void clickBt_map(View v){
+
+        new Thread(r).start();
      web_dash.setVisibility(View.INVISIBLE);
       game_fm.setVisibility(View.INVISIBLE);
       lay_map.setVisibility(View.VISIBLE);
 
     }
+    Runnable r= new Runnable() {
+        @Override
+        public void run() {
 
+                    //좌표를 가지고옴
+                    locationTask=new LocationTask("http://70.12.114.140/android/location.jsp");
+
+                    locationTask.execute(slat,slog);
+
+        }
+    };
+//지도관련 메소드
     private void requestMyLocation() {
         LocationManager manager =
                 (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -129,10 +152,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
     private void showCurrentLocation(Location location) {
         LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+
+        slat=location.getLatitude();
+        slog=location.getLongitude();
         if (mOpt == null) {
             mOpt = new MarkerOptions();
         }
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 14));
     }
 
 
@@ -151,8 +177,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        requestMyLocation();
     }
+    //지도관련 메소드 끝
+
+    //위치정보 보내고 주변 볼링장 정보 받아오기
+    class LocationTask extends AsyncTask<Double, String, String> {
+        String url;
+        //constructor
+        public LocationTask(){
+
+        }
+        public LocationTask(String url){
+            this.url = url;
+        }
+
+        @Override
+        protected String doInBackground(Double... doubles) {
+            double lat=doubles[0];
+            double log=doubles[1];
+            url += "?lat="+lat+"&log="+log;
+            //HTTP REQUEST
+            StringBuilder sb = new StringBuilder();
+            URL url;
+            HttpURLConnection con = null;
+            try{
+                url = new URL(this.url);
+                //Connection
+                con = (HttpURLConnection)url.openConnection();
+                if(con != null){
+                    con.setConnectTimeout(5000);
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("Accept","*/*");
+                    if(con.getResponseCode()!=HttpURLConnection.HTTP_OK){
+                        return null;
+                    }
+                }
+            }catch (Exception e){
+                return e.getMessage();
+
+            }finally {
+                con.disconnect();
+            }
+            return "";
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
 }
