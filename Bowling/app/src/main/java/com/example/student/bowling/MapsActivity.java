4a9da1782cf0071d9b29901df9bcf32245cd8f19
M140310\student <hk041280@gmail.com>
2018-04-19 14:13:34 +0900

    layout

---
+++ Bowling/app/src/main/java/com/example/student/bowling/MapsActivity.java
@@ -0,0 +1,158 @@
+package com.example.student.bowling;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LinearLayout second;
+    RelativeLayout first;
+    LinearLayout lay_map;
+    WebView web_dash;
+    FrameLayout game_fm;
+    private MarkerOptions mOpt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        first=findViewById(R.id.first);
+        second=findViewById(R.id.second);
+        web_dash=findViewById(R.id.web_dash);
+        game_fm=findViewById(R.id.game_fm);
+        lay_map=findViewById(R.id.lay_map);
+    }
+    public void clickBt_login(View v){
+        //로그인 버튼
+    }
+    public void clickBt_register(View v){
+
+    }
+    public void clickBt_map(View v){
+     web_dash.setVisibility(View.INVISIBLE);
+      game_fm.setVisibility(View.INVISIBLE);
+      lay_map.setVisibility(View.VISIBLE);
+
+    }
+
+    private void requestMyLocation() {
+        LocationManager manager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            long minTime = 10000;
+            float minDistance = 0;
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime,
+                    minDistance,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            showCurrentLocation(location);
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    }
+            );
+
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null) {
+                showCurrentLocation(lastLocation);
+            }
+
+            manager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    minTime,
+                    minDistance,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            showCurrentLocation(location);
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    }
+            );
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+    private void showCurrentLocation(Location location) {
+        LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+        if (mOpt == null) {
+            mOpt = new MarkerOptions();
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+    }
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
